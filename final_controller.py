d3a9d35188e163f61da8b414cd9ee441bc053085
Neville <101825590+Neville-Jijina@users.noreply.github.com>
2026-09-10 17:08:16 -0600

    Update final_controller.py

--- final_controller.py
+++ final_controller.py
@@ -1,3 +1,5 @@
+"""epuck_lab1_code controller."""
+
 from controller import Robot, DistanceSensor, Motor
 
 # time in [ms] of a simulation step
@@ -46,6 +48,15 @@ turnCounter = 0
 # keeps track of whether robot is currently going around the box
 leftWallWasSeen = False
 
+#Light sensor reading 
+light_seen = 75
+
+def light_found(lsValues):
+    light_sensors = [0,1,2,5,6,7]
+    return all(lsValues[i] < light_seen for i in light_sensors)
+
+
+
 # feedback loop: step simulation until receiving an exit event
 while robot.step(TIME_STEP) != -1:
     # read sensors outputs
@@ -56,39 +67,68 @@ while robot.step(TIME_STEP) != -1:
     lsValues = []
     for i in range(8):
         lsValues.append(ls[i].getValue())
+    
+    
+    light_detected = light_found(lsValues)
         
     # state machine 
-    if currentState == "FOLLOW_LEFT" : 
+    if currentState == "FOLLOW_LEFT":
+    
+        if light_detected:
+            currentState = "TURN_180"
+            print("Light Source #1 Found!")
+            print("Starting Right Wall Follow")
+            turnCounter = 0
+            vL = 0.0
+            vR= 0.0
         # remember when there is wall on the left
-        if psValues[5] > 70:
+        if psValues[5] > 100:
             leftWallWasSeen = True
             
-        if psValues[0] > 100 or psValues[7] > 100: 
+        if psValues[0] > 120 or psValues[7] > 120: 
             #if there is an obstacle in the front (the end of the wall)
             currentState = "RIGHT_TURN"
             turnCounter = 0 #reset to 0
             
-        elif psValues[5] < 60 and psValues[6] < 70:
-            currentState = "LEFT_TURN"
-            turnCounter = 0
+        elif psValues[5] > 200:
+            #this range is too close to left wall 
+            #move robot to the right 
+            vL = 0.5 * MAX_SPEED
+            vR = -0.2 * MAX_SPEED
+        elif psValues[5] > 230:
+            #this range is too close to left wall 
+            #move robot to the right 
+            vL = 0.5 * MAX_SPEED
+            vR = -0.5 * MAX_SPEED
+            
+        elif psValues[5] < 150 and psValues[5] > 120:
+            vL = 0.5 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+            
+        elif psValues[5] < 120 and psValues[5] > 80:
+            #turn left
+            vL = 0.4 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+            
+        elif psValues[5] < 80 and psValues[5] > 70:
+            #this range is too far from left wall 
+            #move robot to the left
+            # currentState = "LEFT_TURN"
+            vL = 0.4 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+            
+        elif (psValues[6] < 70 and psValues[5] < 70):
+            if leftWallWasSeen:
+               vL = -0.5 * MAX_SPEED
+               vR = 0.5 * MAX_SPEED
+            
+            
+        elif psValues[5] < 70:
+            #go straight till you find something
+            if leftWallWasSeen == False:
+                vL = 0.5 * MAX_SPEED
+                vR = 0.5 * MAX_SPEED
 
-            # reset so it doesn't immediately turn left again
-            leftWallWasSeen = False
-           
-            
-        
-        # elif psValues[5]> 160 :
-            # #this range is too close to left wall 
-            # #move robot to the right 
-            # vL = 0.5 * MAX_SPEED
-            # vR = 0.3 * MAX_SPEED
-            
-        # eliif psValues[5] < 120:
-            # #this range is too far from left wall 
-            # #move robot to the left 
-            # vL = 0.3 * MAX_SPEED
-            # vR = 0.5 * MAX_SPEED
-            
         else: 
             #go straight
             vL = 0.5 * MAX_SPEED
@@ -102,9 +142,6 @@ while robot.step(TIME_STEP) != -1:
         
         turnCounter += 1
         
-        #finish turn after 12 steps 
-        # if psValues[4] > 130 and psValues[3] > 130:
-            # currentState = "FOLLOW_LEFT"
         if turnCounter >= 12 :
             currentState = "FOLLOW_LEFT"
             turnCounter = 0
@@ -117,17 +154,104 @@ while robot.step(TIME_STEP) != -1:
         if turnCounter >= 12 :
             currentState = "FOLLOW_LEFT"
             turnCounter = 0
+            
+    elif currentState == "RIGHT_TURN_RIGHT" : 
+        # turn right!
+        print("Right Turn")
+        vL = 0.5 * MAX_SPEED
+        vR = -0.5 * MAX_SPEED
         
-    elif currentState == "TURN_180": 
-        pass
-        # if lsValues[0] < 10 and lsValues[1] < 10 and lsValues[2] < 10 and lsValues[3] < 10 and lsValues[6] < 10 and lsValues[7] < 10 and lsValues[8] < 10:
-            # vL = 0.5 * MAX_SPEED
-            # vR = -0.5 * MAX_SPEED
-            # if turnCounter >= 18 :
-               # currentState = "FOLLOW_RIGHT"
-               # turnCounter = 0
-    elif currentState == "FOLLOW_RIGHT": 
-         pass 
+        turnCounter += 1
+        
+        if turnCounter >= 12 :
+            currentState = "FOLLOW_RIGHT"
+            turnCounter = 0
+            
+    elif currentState == "LEFT_TURN_RIGHT":
+        print("Turning LEFT")
+        vL = -0.5 * MAX_SPEED
+        vR = 0.5 * MAX_SPEED
+        
+        turnCounter += 1
+        if turnCounter >= 12 :
+            currentState = "FOLLOW_RIGHT"
+            turnCounter = 0
+        
+    elif currentState == "TURN_180":
+        print("Turning 180...")
+        vL = 0.5 * MAX_SPEED
+        vR = -0.5 * MAX_SPEED
+
+        turnCounter += 1
+
+        if turnCounter >= 24:
+            currentState = "FOLLOW_RIGHT"
+            turnCounter = 0
+    
+    elif currentState == "FOLLOW_RIGHT":
+    
+        if light_detected:
+            currentState = "STOP"
+            print("Light Source #2 Found!")
+            print("Found second light source, Job Complete!")
+            turnCounter = 0
+            vL = 0.0
+            vR= 0.0
+        # remember when there is wall on the left
+        if psValues[2] > 100:
+            rightWallWasSeen = True
+            
+        if psValues[0] > 120 or psValues[7] > 120: 
+            #if there is an obstacle in the front (the end of the wall)
+            currentState = "LEFT_TURN_RIGHT"
+            turnCounter = 0 #reset to 0
+            
+        elif psValues[2] > 200:
+            #this range is too close to left wall 
+            #move robot to the right 
+            vL = -0.2 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+        elif psValues[2] > 230:
+            #this range is too close to left wall 
+            #move robot to the right 
+            vL = -0.5 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+            
+        elif psValues[2] < 150 and psValues[2] > 120:
+            vL = 0.5 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+            
+        elif psValues[2] < 120 and psValues[2] > 80:
+            #turn right
+            vL = 0.5 * MAX_SPEED
+            vR = 0.4 * MAX_SPEED
+            
+        elif psValues[2] < 80 and psValues[2] > 70:
+            #this range is too far from left wall 
+            #move robot to the left
+            # currentState = "LEFT_TURN"
+            vL = 0.5 * MAX_SPEED
+            vR = 0.4 * MAX_SPEED
+            
+        elif (psValues[1] < 70 and psValues[2] < 70):
+            if rightWallWasSeen:
+               print("MAX left turn")
+               vL = 0.5 * MAX_SPEED
+               vR = -0.5 * MAX_SPEED
+            
+            
+        elif psValues[2] < 70:
+            #go straight till you find something
+            if rightWallWasSeen == False:
+                vL = 0.5 * MAX_SPEED
+                vR = 0.5 * MAX_SPEED
+
+        else: 
+            #go straight
+            vL = 0.5 * MAX_SPEED
+            vR = 0.5 * MAX_SPEED
+    
+    
     elif currentState == "STOP" : 
         vL = 0
         vR = 0
